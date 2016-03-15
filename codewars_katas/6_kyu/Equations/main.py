efff2965e5c4461a587ad4d75a29c13ef7692153
lozoxmac <maciej.lozowski@tieto.com>
2016-03-15 09:27:29 +0100

    Equations exercise

--- codewars_katas/6_kyu/Equations/main.py
+++ codewars_katas/6_kyu/Equations/main.py
@@ -2,10 +2,9 @@ import math
 def solution(n, m):
     result = 0
     for a in range(int(math.ceil(max([math.sqrt(n), math.sqrt(m)]))+1)):
-        if (a + ((n-a*a)**2)) == m:
-            result+=1
+        if (a + (n-a**2)**2) == m and (a**2 + int(math.sqrt(m-a)) == n):
+                result+=1
     return result
-
 def main():
     print solution(4, 20)
 
